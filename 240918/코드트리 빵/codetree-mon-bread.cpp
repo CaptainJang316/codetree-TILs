afe75df55c3d23cb7ce97778dcafc352d8227364
CaptainJang316 <86219589+CaptainJang316@users.noreply.github.com>
2024-09-18 15:30:21 +0900

    Title: 코드트리 빵, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240918/코드트리 빵/codetree-mon-bread.cpp
+++ 240918/코드트리 빵/codetree-mon-bread.cpp
@@ -1,51 +1,27 @@
-// 12:57 ~ 1:02, 1:31 ~ 3:13, 11:50 ~ 12:05, 12:43 ~ 
-
-// 빵을 구하고자 하는 m명의 사람이 있는데, 1번 사람은 정확히 1분에, 2번 사람은 정확히 2분에, ..., 
-// m번 사람은 정확히 m 분에 각자의 베이스캠프에서 출발하여 편의점으로 이동하기 시작합니다. 
-// 
-// 사람들은 출발 시간이 되기 전까지 '격자 밖에 나와있으며', 사람들이 목표로 하는 편의점은 '모두 다릅니다'. 이 모든 일은 n*n 크기의 격자 위에서 진행됩니다.
-
-// 코드트리 빵을 구하고 싶은 사람들은 다음과 같은 방법으로 움직입니다. 
-// 이 3가지 행동은 '총 1분 동안 진행되며', 정확히 1, 2, 3 순서로 진행되어야 함에 유의합니다.
-
-// 1. 격자에 있는 사람들 모두가 본인이 가고 싶은 편의점 방향을 향해서 1 칸 움직입니다. 
-// '최단거리로 움직이며' 최단 거리로 움직이는 방법이 '여러가지라면' ↑, ←, →, ↓ 의 '우선 순위로' 움직이게 됩니다. 
-
-// 2. 만약 편의점에 '도착한다면' 해당 편의점에서 '멈추게 되고', 이때부터 다른 사람들은 해당 편의점이 있는 칸을 '지나갈 수 없게 됩니다'. 
-// ** 격자에 있는 사람들이 모두 이동한 '뒤에' 해당 칸을 지나갈 수 없어짐에 유의합니다. <-- 남은 사람들의 해당 이동 턴이 끝날 때까지는 통행 가능!
-
-// 3. 현재 시간이 t분이고 t ≤ m를 만족한다면, t번 사람은 자신이 가고 싶은 편의점과 가장 가까이 있는 베이스 캠프에 들어갑니다. <-- 각 편의점별로 가장 가까운 베캠 정보 처음에 저장ㅇㅇ
-// 여기서 가장 가까이에 있다는 뜻 역시 최단거리에 해당하는 곳을 의미합니다. 
-// 가장 가까운 베이스캠프가 '여러 가지인 경우'에는 그 중 '행이 작은 베이스캠프', 행이 같다면 '열이 작은 베이스 캠프'로 들어갑니다. 
-// t번 사람이 베이스 캠프로 이동하는 데에는 '시간이 전혀 소요되지 않습니다.'
-
-// '이때부터' '다른 사람들은' 해당 베이스 캠프가 있는 칸을 '지나갈 수 없게 됩니다.' 
-// t번 사람이 편의점을 향해 움직이기 시작했더라도 해당 베이스 캠프는 '앞으로 절대 지나갈 수 없음'에 유의합니다. 
-// 마찬가지로 해당 턴 격자에 있는 사람들이 모두 이동한 뒤에 해당 칸을 지나갈 수 없어짐에 유의합니다.
-
-
-// 이미 사람들이 도착한 편의점이나 출발한 적이 있는 베이스캠프의 경우 움직일 때 '절대 지나갈 수 없는 공간'임을 유의합니다. (예시에서는 빨간색으로 표시되어 있습니다.)
-
-// 도달 못하는 경우는 없음.
-
-// 또한, 이동하는 도중 동일한 칸에 둘 이상의 사람이 위치하게 되는 경우 역시 '가능함'에 유의합니다.
-
-
-//첫 번째 줄에는 격자의 크기 n과 사람의 수 m이 공백을 사이에 두고 주어집니다.
-//
-//이후 n개의 줄에 걸쳐 격자의 정보가 주어집니다.각 줄에 각각의 행에 해당하는 n개의 수가 공백을 사이에 두고 주어집니다.
-//0의 경우에는 빈 공간, 1의 경우에는 베이스캠프를 의미합니다.
-//
-//이후 m개의 줄에 걸쳐 각 사람들이 가고자 하는 편의점 위치의 행 x, 열 y의 정보가 공백을 사이에 두고 주어집니다.
-//
-//각 사람마다 가고 싶은 편의점의 위치는 겹치지 않으며, 편의점의 위치와 베이스캠프의 위치도 겹치지 않습니다.
-
+// 12:57 ~ 1:02, 1:31 ~ 3:13, 11:50 ~ 12:05, 12:43 ~ 1:56 <-- 3시간 20분 정도 소요.. 그마저도 틀린 테케가 뭔지 알 수 있었던 덕분;;
+// => 다시 풀어볼 것!
 
 // ** input으로 받는 r,c값이 0부터인지 1부터인지 확인해야 함.. 나도 알고 싶지 않았음ㅋㅋㅋ...
+
+// ** 온갖 종류의 BFS를 다 모아놓은 문제
+
+// ** 도착지 기준으로 가까운 곳으로 '한칸만 이동하되', 같은 거리는 방향 우선순위 있는 경우
+//    => 우선순위 방향대로, 처음 출발한 방향을 계속 r,c와 함께 cd 값으로 q에 계속 넘겨준다. <- 가장 먼저 발견되는 경우의 cd방향으로 결정
+//       (이를 위해 처음 nr들에 대해선 별도의 for문 처리ㅇㅇ)
+
+// ** 목적지 기준으로 가장 가까운 출발지 후보들 목록을 우선순위로 지정하려 했으나, 이 문제에선 잘못된 방법이었다.
+//    (아직 map이 깨끗한 최초 상태 기준으로 가까워도, 로직이 진행되며 길이 막히게 되거나 돌아가야 하게 바뀔 수 있는 요소 존재)
+//    => 그냥 매번 목적지 기준으로 가장 가까운 출발지(baseCamp)를 찾아야 한다. <-- 매번 실행되는 게 아니라, 각 사람마다 1번씩만 시행되므로, 괜찮다!
+//    (즉, 문제의 로직 구조를 이해하고 이에 맞는 방식으로 구현할 것ㅇㅇㅇ)
+
+// ** 한칸 이동 말고, 최단거리 목적지만 찾되 같은 거리에 있는 경우, 행과 열의 크고 작음으로 우선순위 두는 경우
+//    => 무조건 depth 써야 한다. 방향 설정만으론 절대 안된다.(이게 되는 건 한칸만 이동하는 경우ㅇㅇ)
+//       depth가 같은 것들을 pq에 넣고 첫번째 것만 꺼내서 그걸로 선택하면 됨. 
 
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
@@ -73,48 +49,49 @@ vector<vector<int>> map(16, vector<int>(16));
 queue< SpotInfo> aq;
 vector< Person> p(31);
 vector< Spot> mk(31);
-vector<vector< Spot>> nBC(31);
 
 bool isOutOfBound(int r, int c) {
     return r <= 0 || n < r || c <= 0 || n < c;
 }
 
-void getBaseCampDistBFS() {
-    queue<Spot> q;
-    vector<vector<int>> visited(16, vector<int>(16, 0));
-    for (int i = 1; i <= m; i++)
-    {
-        q.push({ mk[i].r, mk[i].c });
-        visited[mk[i].r][mk[i].c] = i;
-
-        while (!q.empty())
-        {
-            int r = q.front().r;
-            int c = q.front().c;
-            q.pop();
-
-            for (int d = 0; d < 4; d++)
-            {
-                int nr = r + dr[d];
-                int nc = c + dc[d];
-
-                if (isOutOfBound(nr, nc)) continue;
-
-                if (map[nr][nc] == 1 && visited[nr][nc] != i)
-                {
-                    nBC[i].push_back({ nr, nc });
-                    visited[nr][nc] = i;
-                    q.push({ nr, nc });
-                }
-                else if(visited[nr][nc] != i)
-                {
-                    visited[nr][nc] = i;
-                    q.push({ nr, nc });
-                }
-            }
-        }
-    }
-}
+// ** 이렇게 각 목적지별로 가까운 bc들을 미리 찾아놓으려 했으나. 결과적으로 생각해보면, 어차피 시간 소요는 같음(각 사람마다 최초 1번만 시행되므로ㅇㅇ)
+//    그리고, 맵의 이동 불가 영역들이 점점 추가되는 로직에선, 이는 틀린 방법일 수밖에 없음.(최초 상태 기준으로 순위를 매겼으므로ㅇㅇ)
+//void getBaseCampDistBFS() {
+//    queue<Spot> q;
+//    vector<vector<int>> visited(16, vector<int>(16, 0));
+//    for (int i = 1; i <= m; i++)
+//    {
+//        q.push({ mk[i].r, mk[i].c });
+//        visited[mk[i].r][mk[i].c] = i;
+//
+//        while (!q.empty())
+//        {
+//            int r = q.front().r;
+//            int c = q.front().c;
+//            q.pop();
+//
+//            for (int d = 0; d < 4; d++)
+//            {
+//                int nr = r + dr[d];
+//                int nc = c + dc[d];
+//
+//                if (isOutOfBound(nr, nc)) continue;
+//
+//                if (map[nr][nc] == 1 && visited[nr][nc] != i)
+//                {
+//                    nBC[i].push_back({ nr, nc });
+//                    visited[nr][nc] = i;
+//                    q.push({ nr, nc });
+//                }
+//                else if(visited[nr][nc] != i)
+//                {
+//                    visited[nr][nc] = i;
+//                    q.push({ nr, nc });
+//                }
+//            }
+//        }
+//    }
+//}
 
 void moveBFS(int i, vector<vector<int>> &visited) {
     queue<SpotInfo> q;
@@ -130,7 +107,9 @@ void moveBFS(int i, vector<vector<int>> &visited) {
         if (nr == mk[i].r && nc == mk[i].c)
         {
             p[i].isArrive = true;
-            aq.push({ nr,nc });
+            p[i].r = nr;
+            p[i].c = nc;
+            aq.push({ nr,nc,i });
             return;
         }
         else if (map[nr][nc] <= 1) {
@@ -152,7 +131,7 @@ void moveBFS(int i, vector<vector<int>> &visited) {
 
             if (isOutOfBound(nr, nc)) continue;
 
-            if (nr == mk[i].r && c == mk[i].c)
+            if (nr == mk[i].r && nc == mk[i].c)
             {
                 p[i].r += dr[cd];
                 p[i].c += dc[cd];
@@ -164,6 +143,62 @@ void moveBFS(int i, vector<vector<int>> &visited) {
             }
         }
     }
+}
+
+struct cmp
+{
+    bool operator()(Spot& a, Spot& b) {
+        if (a.r == b.r) return a.c > b.c;
+        return a.r > b.r;
+    }
+};
+
+void selectNearBaseCampBFS(int idx) {
+    vector<vector<int>> visited(16, vector<int>(16, 0));
+    queue<SpotInfo> q;
+    priority_queue<Spot, vector<Spot>, cmp> bc_pq;
+    q.push({ mk[idx].r, mk[idx].c, 0 });
+    visited[mk[idx].r][mk[idx].c] = 1;
+    int minDepth = 100000;
+
+    while (!q.empty())
+    {
+        int r = q.front().r;
+        int c = q.front().c;
+        int depth = q.front().v;
+        q.pop();
+
+        // ** 거리가 같은 경우, r&c 값 기준으로 우선순위 정해야 할 때,
+        //    이렇게 minDepth 처리하면 된다.
+        if (minDepth < depth) break; 
+
+        for (int d = 0; d < 4; d++)
+        {
+            int nr = r + dr[d];
+            int nc = c + dc[d];
+
+            if (isOutOfBound(nr, nc)) continue;
+            
+            if (map[nr][nc] == 1)
+            {
+                minDepth = min(depth, minDepth); // <<-- 여기!!!!!
+                bc_pq.push({ nr,nc });
+                visited[nr][nc] = 1;
+            }
+            else if (map[nr][nc] <= 1 && visited[nr][nc] != 1) {
+                q.push({ nr, nc, depth + 1 });
+                visited[nr][nc] = 1;
+            }
+        }
+    }
+
+    int bc_r = bc_pq.top().r; // <<-- 여기!!!!!
+    int bc_c = bc_pq.top().c;
+    bc_pq.pop();
+    p[idx].r = bc_r;
+    p[idx].c = bc_c;
+    map[bc_r][bc_c] = idx + 1;
+    return;
 }
 
 int main() {
@@ -182,8 +217,6 @@ int main() {
         p[i].isArrive = false;
     }
 
-    getBaseCampDistBFS();
-
     int t = 1;
     while (true)
     {
@@ -194,7 +227,6 @@ int main() {
             for (int i = 1; i <= t; i++)
             {
                 if (p[i].isArrive) continue;
-
                 isEnd = false;
                 moveBFS(i, visited);
             }
@@ -204,11 +236,11 @@ int main() {
             for (int i = 1; i <= m; i++)
             {
                 if (p[i].isArrive) continue;
-
                 isEnd = false;
                 moveBFS(i, visited);
             }
         }
+
         while (!aq.empty())
         {
             int r = aq.front().r;
@@ -222,19 +254,7 @@ int main() {
         if (t <= m)
         {
             isEnd = false;
-            for (int i = 0; i < nBC[t].size(); i++)
-            {
-                int r = nBC[t][i].r;
-                int c = nBC[t][i].c;
-
-                if (map[r][c] <= 1)
-                {
-                    p[t].r = r;
-                    p[t].c = c;
-                    map[r][c] = t + 1;
-                    break;
-                }
-            }
+            selectNearBaseCampBFS(t);
         }
         if (isEnd) {
             t--;
@@ -242,7 +262,6 @@ int main() {
         }
         t++;
     }
-
     cout << t << '\n';
     return 0;
 }
